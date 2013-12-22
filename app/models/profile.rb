6d44903bb5c17650329727ca167af4352ba1bab3
macheteshuffle <jeff.li029@gmail.com>
2013-12-22 10:40:58 -0500

    created and passed Profile attributes test

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,8 @@
 class Profile < ActiveRecord::Base
+  validates_presence_of :last_name
+  validates_presence_of :first_name
+  validates_presence_of :username
+  validates :password,  :confirmation => true, :presence => true
+  validates :email,     :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, :presence => true
+
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Profile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:last_names) { ["Smith","Jones","Robinson"] }
+  let(:first_names) { ["Mike","John","Melissa"] }
+  let(:usernames) { ["swole_n_ripped","diesel_as_fuck",'squatqueen']}
+  let(:passwords) { ["2jack3d","@$$hole98","pr3ttynp!nk"] }
+  let(:emails) { ["mike@gmail.com","john@gmail.com","mrobinson@gmail.com"] }
+
+
+  it { should have_valid(:last_name).when(*last_names) }
+  it { should_not have_valid(:last_name).when(nil) }
+  it { should have_valid(:first_name).when(*first_names) }
+  it { should_not have_valid(:last_name).when(nil) }
+  it { should have_valid(:username).when(*usernames) }
+  it { should_not have_valid(:username).when(nil) }
+  it { should have_valid(:password).when(*passwords) }
+  it { should_not have_valid(:password).when(nil) }
+  it { should have_valid(:email).when(*emails) }
+  it { should_not have_valid(:email).when(nil) }
+
+  describe 'password' do
+    before { subject.password_confirmation = *passwords }
+    it { should have_valid(:password).when(*passwords) }
+    it { should_not have_valid(:password).when(nil) }
+  end
+
+
 end
